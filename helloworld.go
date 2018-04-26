3540d5542e7241ba281ddb3d398e56f18f5bf979
zhanghaowei <zhanghaowei@qnvip.com>
2018-04-26 12:29:37 +0800

    use different keys in tencent or aws

--- helloworld.go
+++ helloworld.go
@@ -7,10 +7,11 @@ import (
 	"hello/api"
 	"hello/model"
 	"github.com/jinzhu/configor"
+	"fmt"
 )
 
 func main() {
-	util.Notice("start application")
+	util.SocketInfo("start init")
 	model.NewConfig()
 	err := configor.Load(model.ApplicationConfig, "./config.yml")
 	if err != nil {
@@ -20,12 +21,14 @@ func main() {
 	model.SetApiKeys()
 	model.ApplicationDB, err = gorm.Open("postgres", model.ApplicationConfig.DBConnection)
 	if err != nil {
-		panic(err)
+		util.SocketInfo(fmt.Sprint(err))
+		return
 	}
 	defer model.ApplicationDB.Close()
 	model.ApplicationDB.AutoMigrate(&model.Carry{})
 	model.ApplicationDB.AutoMigrate(&model.Account{})
 
+	util.SocketInfo("start making money")
 	model.HuobiAccountId, _ = api.GetSpotAccountId(model.ApplicationConfig)
 	api.GetAccountHuobi(model.ApplicationAccounts)
 	api.GetAccountOkex(model.ApplicationAccounts)
