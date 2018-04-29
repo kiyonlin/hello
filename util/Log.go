a7305d0e8a246c4ba8b115742c13274088b799de
zhanghaowei <zhanghaowei@qnvip.com>
2018-04-30 06:38:57 +0800

    remove yesterday logs

--- util/Log.go
+++ util/Log.go
@@ -13,6 +13,8 @@ var socket, info, notice *log.Logger
 var socketInfoFile, infoFile, noticeFile *os.File
 var socketCount, infoCount, noticeCount int
 
+const logRoot = "./log/"
+
 func initLog(path string) (*log.Logger, *os.File, error) {
 	removeOldFiles()
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
@@ -25,7 +27,7 @@ func initLog(path string) (*log.Logger, *os.File, error) {
 func removeOldFiles() {
 	year, month, date := GetNow().Date()
 	strDate := strconv.Itoa(year) + month.String() + strconv.Itoa(date)
-	err := filepath.Walk("./log/", func(path string, f os.FileInfo, err error) error {
+	err := filepath.Walk(logRoot, func(path string, f os.FileInfo, err error) error {
 		if f == nil {
 			return err
 		}
@@ -34,9 +36,9 @@ func removeOldFiles() {
 		}
 		fmt.Printf(path)
 		if !strings.Contains(f.Name(), strDate) {
-			rmErr := os.Remove("." + path + "/" + f.Name())
+			rmErr := os.Remove(logRoot + f.Name())
 			if rmErr != nil {
-				fmt.Println("." + path + "/" + f.Name() + "can not remove " + rmErr.Error())
+				fmt.Println(logRoot + f.Name() + "can not remove " + rmErr.Error())
 			}
 		}
 		return nil
@@ -50,7 +52,7 @@ func getPath(name string) string {
 	year, month, date := GetNow().Date()
 	strDate := strconv.Itoa(year) + month.String() + strconv.Itoa(date)
 	strTime := strconv.Itoa(GetNow().Hour()) + "_" + strconv.Itoa(GetNow().Minute())
-	return "./log/" + name + strDate + "_" + strTime + ".log"
+	return logRoot + name + strDate + "_" + strTime + ".log"
 }
 
 func SocketInfo(message string) {
