d62f720329624dace052dbe117aebc75171889a5
haoweizh@qq.com <haoweizh@qq.com>
2018-08-02 08:44:58 +0800

    test binance ws

--- api/Binance.go
+++ api/Binance.go
@@ -20,6 +20,7 @@ var subscribeHandlerBinance = func(subscribes []string, conn *websocket.Conn) er
 
 func WsDepthServeBinance(markets *model.Markets, carryHandlers []CarryHandler, errHandler ErrHandler) (chan struct{}, error) {
 	wsHandler := func(event []byte, conn *websocket.Conn) {
+		util.Notice(string(event))
 		json, err := util.NewJSON(event)
 		if err != nil {
 			errHandler(err)
