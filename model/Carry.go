e49d1f092d028509bf03f6d1becb76cdf7f5c40f
haoweizh@qq.com <haoweizh@qq.com>
2018-10-08 23:00:52 +0800

    arbitrary

--- model/Carry.go
+++ model/Carry.go
@@ -40,9 +40,6 @@ type Carry struct {
 	SideType  string
 }
 
-func getLastFutureCarry()  {
-
-}
 func getDynamicMargin(carry *Carry, configMargin float64) (dynamicMargin float64) {
 	if AppAccounts == nil {
 		return configMargin
