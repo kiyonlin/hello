8dea7751a2909986c559faa88be90ccd0bf8669d
张浩伟 <haoweizh@gmail.com>
2019-06-13 22:35:29 +0800

    delay time slot for 5s

--- carry/Refresh.go
+++ carry/Refresh.go
@@ -233,7 +233,7 @@ func (refreshOrders *RefreshOrders) CheckAmountLimit(market, symbol string, amou
 	defer refreshOrders.lock.Unlock()
 	refreshOrders.lock.Lock()
 	now := util.GetNow()
-	amountIndex = int((now.Hour()*3600 + now.Minute()*60 + now.Second()) / model.RefreshTimeSlot)
+	amountIndex = int((now.Hour()*3600 + now.Minute()*60 + now.Second() - 5) / model.RefreshTimeSlot)
 	if refreshOrders.amountLimit == nil || refreshOrders.amountLimit[market] == nil ||
 		refreshOrders.amountLimit[market][symbol] == nil {
 		return true, amountIndex
