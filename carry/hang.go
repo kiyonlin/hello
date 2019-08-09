df86baf8c27f0d1bc7a895c03ffa95388ef2e230
张浩伟 <haoweizh@gmail.com>
2019-08-09 18:27:27 +0800

    bid ask 1 sequence hang

--- carry/hang.go
+++ carry/hang.go
@@ -90,62 +90,38 @@ var ProcessHang = func(market, symbol string) {
 	if bid == nil || bid.Price > tick.Bids[0].Price {
 		didSmth = true
 		bid = api.PlaceOrder(``, ``, model.OrderSideBuy, model.OrderTypeLimit, market, symbol,
-			setting.AccountType, setting.AccountType, tick.Bids[0].Price,
-			math.Min(tick.Bids[0].Amount, rightFree*0.9/tick.Bids[0].Price))
+			setting.AccountType, setting.AccountType, tick.Bids[2].Price,
+			math.Min(tick.Bids[2].Amount, rightFree*0.9/tick.Bids[2].Price))
 		if bid != nil && bid.OrderId != `` {
 			bid.Function = model.FunctionHang
 			model.AppDB.Save(&bid)
 			hangStatus.setBid(symbol, bid)
 		}
 	} else {
-		if bid.Price < tick.Bids[0].Price {
+		if bid.Price < tick.Bids[5].Price || bid.Price > tick.Bids[1].Price {
 			didSmth = true
-			util.Notice(fmt.Sprintf(`---0 cancel bid not on 1 %f < %f`, bid.Price, tick.Bids[0].Price))
+			util.Notice(fmt.Sprintf(`---0 cancel bid %f < %f or > %f`,
+				bid.Price, tick.Bids[5].Price, tick.Bids[1].Price))
 			api.MustCancel(``, ``, market, symbol, bid.OrderId, true)
 			hangStatus.setBid(symbol, nil)
-		} else if bid.Price == tick.Bids[0].Price {
-			bid = api.QueryOrderById(``, ``, market, symbol, bid.OrderId)
-			if bid != nil {
-				if bid.DealAmount > 0 && bid.Status == model.CarryStatusWorking {
-					didSmth = true
-					util.Notice(fmt.Sprintf(`---1 check bid price %f = %f amount %f > deal %f`,
-						bid.Price, tick.Bids[0].Price, bid.Amount, bid.DealAmount))
-					api.MustCancel(``, ``, market, symbol, bid.OrderId, false)
-					hangStatus.setBid(symbol, nil)
-				} else if bid.Status == model.CarryStatusSuccess || bid.Status == model.CarryStatusFail {
-					hangStatus.setBid(symbol, nil)
-				}
-			}
 		}
 	}
 	if ask == nil || ask.Price < tick.Asks[0].Price {
 		didSmth = true
 		ask = api.PlaceOrder(``, ``, model.OrderSideSell, model.OrderTypeLimit, market, symbol,
-			setting.AccountType, setting.AccountType, tick.Asks[0].Price, math.Min(tick.Asks[0].Amount, leftFree*0.9))
+			setting.AccountType, setting.AccountType, tick.Asks[2].Price, math.Min(tick.Asks[2].Amount, leftFree*0.9))
 		if ask != nil && ask.OrderId != `` {
 			ask.Function = model.FunctionHang
 			model.AppDB.Save(&ask)
 			hangStatus.setAsk(symbol, ask)
 		}
 	} else {
-		if ask.Price > tick.Asks[0].Price {
+		if ask.Price > tick.Asks[5].Price || ask.Price < tick.Asks[1].Price {
 			didSmth = true
-			util.Notice(fmt.Sprintf(`---0 cancel ask not on 1 %f > %f`, ask.Price, tick.Asks[0].Price))
+			util.Notice(fmt.Sprintf(`---0 cancel ask %f > %f or < %f`,
+				ask.Price, tick.Asks[5].Price, tick.Asks[1].Price))
 			api.MustCancel(``, ``, market, symbol, ask.OrderId, true)
 			hangStatus.setAsk(symbol, nil)
-		} else if ask.Price == tick.Asks[0].Price {
-			ask = api.QueryOrderById(``, ``, market, symbol, ask.OrderId)
-			if ask != nil {
-				if ask.DealAmount > 0 && ask.Status == model.CarryStatusWorking {
-					didSmth = true
-					util.Notice(fmt.Sprintf(`---1 check ask price %f = %f amount %f > deal %f`,
-						ask.Price, tick.Asks[0].Price, ask.Amount, ask.DealAmount))
-					api.MustCancel(``, ``, market, symbol, ask.OrderId, false)
-					hangStatus.setAsk(symbol, nil)
-				} else if ask.Status == model.CarryStatusSuccess || ask.Status == model.CarryStatusFail {
-					hangStatus.setAsk(symbol, nil)
-				}
-			}
 		}
 	}
 	if didSmth {
