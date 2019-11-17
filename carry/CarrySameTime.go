a2b3025eadb4533e70fcb2a1e7286da8a6efa536
张浩伟 <haoweizh@gmail.com>
2019-11-17 10:24:15 +0800

    carry same time between bm & fm

--- carry/CarrySameTime.go
+++ carry/CarrySameTime.go
@@ -107,7 +107,7 @@ func placeBothOrders(symbol string, tickBM, tickFM *model.BidAsk, accountBM, acc
 	//	tickFM.Bids[0].Amount, tickFM.Asks[0].Amount))
 	if fmb1-tickBM.Bids[0].Price >= setting.GridPriceDistance-p1 && fmba >= setting.RefreshLimitLow &&
 		tickBM.Bids[0].Amount*10 < tickBM.Asks[0].Amount && tickBM.Asks[0].Amount > 800000 {
-		amount := math.Min(math.Min(fmba*0.8, a1), setting.GridAmount)
+		amount := math.Min(math.Min(fmba*0.5, a1), setting.GridAmount)
 		if amount > 1 {
 			go api.PlaceOrder(``, ``, model.OrderSideSell, model.OrderTypeLimit, model.Fmex,
 				symbol, ``, setting.AccountType, calcAmtPriceBuy, amount, true)
@@ -122,7 +122,7 @@ func placeBothOrders(symbol string, tickBM, tickFM *model.BidAsk, accountBM, acc
 		}
 	} else if tickBM.Asks[0].Price-fms1 >= setting.GridPriceDistance-p2 && fmsa >= setting.RefreshLimitLow &&
 		tickBM.Asks[0].Amount*10 < tickBM.Bids[0].Amount && tickBM.Bids[0].Amount > 800000 {
-		amount := math.Min(math.Min(fmsa*0.8, a2), setting.GridAmount)
+		amount := math.Min(math.Min(fmsa*0.5, a2), setting.GridAmount)
 		if amount > 1 {
 			go api.PlaceOrder(``, ``, model.OrderSideBuy, model.OrderTypeLimit, model.Fmex,
 				symbol, ``, setting.AccountType, calcAmtPriceSell, amount, true)
