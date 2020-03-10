a5c417c0b6e1469905057320df32d6e348120ac6
张浩伟 <haoweizh@gmail.com>
2020-03-11 00:32:23 +0800

    update handler parameters

--- carry/CarrySameTime.go
+++ carry/CarrySameTime.go
@@ -171,10 +171,10 @@ func placeBothOrders(market, symbol, key string, tick, tickRelated *model.BidAsk
 	} else if priceX < -7 {
 		priceX = priceX/2 - 3.5
 	}
-	model.SetCarryInfo(fmt.Sprintf(`%s_%s_%s`, model.FunctionCarry, market, setting.MarketRelated),
-		fmt.Sprintf("[搬砖参数] %s %s资金费率:%f %s资金费率%f p1:%f p2:%f py:%f px:%f 对应持仓:%f %s 延时 %dms\n"+
-			"【设置】下单数量: %f, 价差参数:%f,  A总: %f, 最低数量: %f\n"+
-			"%d-%d %f %f %f %f %f - %f %f %f %f %f",
+	model.SetCarryInfo(fmt.Sprintf("【%s_%s_%s】\n", model.FunctionCarry, market, setting.MarketRelated),
+		fmt.Sprintf("1. [搬砖参数] %s %s资金费率:%f %s资金费率%f p1:%f p2:%f py:%f px:%f 对应持仓:%f %s 延时 %dms\n"+
+			"2. [设置]下单数量: %f, 价差参数:%f,  A总: %f, 最低数量: %f\n"+
+			"3. %d-%d %f %f %f %f %f - %f %f %f %f %f",
 			util.GetNow().String(), market, zFee, setting.MarketRelated, zFeeRelated,
 			p1, p2, py, priceX, freeRelated, util.GetNow().String(), util.GetNowUnixMillion()-int64(tick.Ts),
 			// refreshLimitLow 限制
