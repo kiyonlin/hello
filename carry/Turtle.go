f9376c398f570c639ced3276daf0972b708c0408
张浩伟 <haoweizh@gmail.com>
2020-03-28 14:44:55 +0800

    add ftx for turtle platform coin

--- carry/Turtle.go
+++ carry/Turtle.go
@@ -46,7 +46,7 @@ func calcTurtleAmount(market, symbol string, price, n float64) (amount float64) 
 			amount *= 2
 		case `ethusd_p`, `eosusd_p`:
 			amount *= 1.5
-		case `htusd_p`, `okbusd_p`, `bnbusd_p`:
+		case `htusd_p`, `okbusd_p`, `bnbusd_p`, `btmxusd_p`:
 			amount *= 0.5
 		}
 	}
